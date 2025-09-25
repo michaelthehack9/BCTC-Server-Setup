256d5810eb39ab024b2d3a62603eddbd8a6a7abf
michaelthehack9 <77589350+michaelthehack9@users.noreply.github.com>
2025-09-25 14:40:52 -0400

    Add files via upload

---
+++ Controls.bat
@@ -0,0 +1,45 @@
+@echo off
+call :banner
+call :showList
+call :getInput
+pause
+exit /B 0
+
+:banner
+echo:
+echo:
+echo:
+echo  ________      ________    ________      ________      ___  ___      _______
+echo ^|\   __  \    ^|\   __  \  ^|\   __  \    ^|\   ____\    ^|\  \^|\  \    ^|\  ___ \
+echo \ \  \^|\  \   \ \  \^|\  \ \ \  \^|\  \   \ \  \___^|    \ \  \\\  \   \ \   __/^|
+echo  \ \   __  \   \ \   ____\ \ \   __  \   \ \  \        \ \   __  \   \ \  \_^|/__
+echo   \ \  \ \  \   \ \  \___^|  \ \  \ \  \   \ \  \____    \ \  \ \  \   \ \  \_^|\ \
+echo    \ \__\ \__\   \ \__\      \ \__\ \__\   \ \_______\   \ \__\ \__\   \ \_______\
+echo     \^|__^|\^|__^|    \^|__^|       \^|__^|\^|__^|    \^|_______^|    \^|__^|\^|__^|    \^|_______^|
+echo:
+echo:
+echo:
+exit /B 0
+:showList
+echo:
+echo:
+echo:
+echo ------ 1) ^Start
+echo ---- 2) ^Stop
+exit /B 0
+
+:getInput
+set /p input=--^> 
+if %input% == 1 call :startAll
+if %input% == 2 call :stopAll
+exit /B 0
+
+:startAll
+%MYSQL_BIN%\hideMysqld.vbs
+%APACHE_BIN%\hideApache.vbs
+exit /B 0
+
+:stopAll
+%MYSQL_BIN%\mysqladmin -u server -pserver shutdown
+taskkill /F /im apache.exe
+exit /B 0
